4178b6a04f6edd80e2640880c28d988fb9c28e84
Patricia Delgado <patricia.delgado.morcillo@gmail.com>
2019-04-02 22:08:33 +0200

    starter npm and routes index page, app is running now

--- starter_code/app.js
+++ starter_code/app.js
@@ -1,13 +1,28 @@
 const express = require('express');
 const hbs = require('hbs');
-
-// require spotify-web-api-node package here:
-
-
-
+const SpotifyWebApi = require('spotify-web-api-node');
 
 const app = express();
+const path = require('path');
 
+//----------------------------------------------
+// Remember to insert your credentials here
+const clientId = 'b428a17f314347589a483ab7012dc91d',
+      clientSecret = 'ef26d50c4fc2491b96f8e4b3c1254844';
+
+const spotifyApi = new SpotifyWebApi({
+  clientId : clientId,
+  clientSecret : clientSecret
+});
+// Retrieve an access token
+spotifyApi.clientCredentialsGrant()
+  .then( data => {
+    spotifyApi.setAccessToken(data.body['access_token']);
+  })
+  .catch(error => {
+    console.log('Something went wrong when retrieving an access token', error);
+  })
+//----------------------------------------------
 app.set('view engine', 'hbs');
 app.set('views', __dirname + '/views');
 app.use(express.static(__dirname + '/public'));
@@ -22,7 +37,9 @@ app.use(express.static(__dirname + '/public'));
 
 
 // the routes go here:
-
+app.get('/', (req, res, next) => {
+  res.render('index');
+});
 
 
 app.listen(3000, () => console.log("My Spotify project running on port 3000 🎧 🥁 🎸 🔊"));
